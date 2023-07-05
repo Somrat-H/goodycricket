be42760d784b5a04c26f85ef2ef2fc4ee2033dc7
Somrat Hossain <somrat945hossain@gmail.com>
2023-07-05 14:39:02 +0600

    add navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:goodycricket/ui/home_page.dart';
+import 'package:goodycricket/ui/navbar/navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: NavBar(),
     );
   }
 }
