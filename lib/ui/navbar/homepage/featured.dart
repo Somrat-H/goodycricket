09c08717d07cebc3511c09033b3ea607db661080
Somrat Hossain <somrat945hossain@gmail.com>
2023-07-05 14:49:14 +0600

    just did homepage screen

---
+++ lib/ui/navbar/homepage/featured.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class Featured extends StatelessWidget {
+  const Featured({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Featured'),
+      ),
+    );
+  }
+}
