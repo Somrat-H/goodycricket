ff5b49bfe79a6f1223a66bd9acdaaa575f17b95b
Somrat Hossain <somrat945hossain@gmail.com>
2023-07-09 16:27:18 +0600

    add const

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:goodycricket/ui/navbar/navbar.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: NavBar(),
     );
